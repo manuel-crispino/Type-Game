125e253c7951245882789adc05ee5309d6358f9d
Manuel Crispino <manuel.barman96@gmail.com>
2024-02-27 15:48:53 +0100

    port changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs'); // Set EJS as the view engine
 app.set('views', join(__dirname, 'views')); // Set the views directory
